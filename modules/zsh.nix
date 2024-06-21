db8ef4479c188b106c2e226cd8f96313f761e89a
Jaimevzkz <jaimepva@ucm.es>
2024-06-21 18:17:36 +0200

    zsh conf

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -10,7 +10,7 @@
     };
 
     shellAliases = {
-      l = "eza -l --icons --git --sort=Extension";
+      l = "eza -lah --icons --git --sort=Extension";
       v = "nvim";
       e = "exit";
       kt="/home/vzkz/Own_Pojects/kotlin_WorkSpace && eza --icons -lah";
@@ -20,6 +20,14 @@
       cd = "z";
       update = "sudo nixos-rebuild switch --flake ~/nixos/#nixos-config";
       shell = "nix develop --command zsh";
+      lg = "lazygit";
+      psh = ''
+        if [ -z "$1" ]; then
+          echo "Por favor, proporciona un mensaje de commit."
+        else
+          git add . && git commit -m "$1" && git push
+        fi
+      '';
 
     };
     oh-my-zsh = {
